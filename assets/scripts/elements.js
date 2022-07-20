2f210a329c89818c656790701144fe55cb451f65
LAZAROS PAPANIKOLAOU <44327139+lazarospapanikolaou@users.noreply.github.com>
2022-07-20 19:50:14 +0300

    update

--- assets/scripts/elements.js
+++ assets/scripts/elements.js
@@ -1,1 +1,14 @@
+<script>
+  let toggle = button => {
+     let Element = document.getElementById("myvideo");
+     let hidden = element.getAttribute("hidden");
 
+     if (hidden) {
+        element.removeAttribute("hidden");
+        button.innerText = "Hide Video";
+     } else {
+        element.setAttribute("hidden", "hidden");
+        button.innerText = "Show Video";
+     }
+  }
+</script>
