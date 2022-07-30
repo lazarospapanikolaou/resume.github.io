45d45912048405771a7e4c62bb0c1cbc6e2439bf
Lazaros Papanikolaou <lazaros.papanikolaoy@gmail.com>
2022-07-30 14:39:48 +0300

    Update elements.js

--- assets/scripts/elements.js
+++ assets/scripts/elements.js
@@ -1,14 +1,4 @@
-<script>
- z  let toggle = button => {
-     let Element = document.getElementById("myvideo");
-     let hidden = element.getAttribute("hidden");
-
-     if (hidden) {
-        element.removeAttribute("hidden");
-        button.innerText = "Hide Video";
-     } else {
-        element.setAttribute("hidden", "hidden");
-        button.innerText = "Show Video";
-     }
-  }
-</script>
+function toggle(){
+ var videoContainer = document.querySelector(".videoContainer")
+ videoContainer.classList.toggle("active")
+}
